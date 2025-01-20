e97c6b7524cd73fd5c507fd1279ddbd7568f5697
tumminia <tumminia.p@outlook.it>
2025-01-20 15:24:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
 <head>
 <meta name='csrf-token' content='<?php print($object->tokenCsrf()); ?>' charset='UTF-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Git project">
-<link rel="manifest" href="/gitproject/manifest.json" crossorigin="use-credentials" />
+<meta name="description" content="Php project">
+<link rel="manifest" href="/php/manifest.json" crossorigin="use-credentials" />
 <title>CAP di tutte le città Italiane</title>
-<link rel="icon" href="/gitProject/css/git.svg" sizes="32x32">
+<link rel="icon" href="/php/css/git.svg" sizes="32x32">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-<link rel="stylesheet" href="/gitProject/css/index.css">
+<link rel="stylesheet" href="/php/css/index.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-<script type="text/babel" src="/gitProject/js/react.js" charset="UTF-8"></script>
+<script type="text/babel" src="/php/js/react.js" charset="UTF-8"></script>
 </head>
 <body>
 
